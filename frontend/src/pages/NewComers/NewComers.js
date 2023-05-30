3fe0d81d72744f76cddf07da54c065d2260d0652
Junesung <junesoung412@gmail.com>
2023-05-30 00:58:30 -0600

    frontend: newcomer: maxwidth flow

--- frontend/src/pages/NewComers/NewComers.js
+++ frontend/src/pages/NewComers/NewComers.js
@@ -1,6 +1,7 @@
 import ReactFlow, { useNodesState, useEdgesState } from "reactflow";
 
 import "reactflow/dist/style.css";
+import { Typography } from "@mui/material";
 
 const initialNodes = [
   {
@@ -60,7 +61,7 @@ const NewComers = () => {
   return (
     <>
       <h1>새가족</h1>
-      <p style={{ color: "black" }}>
+      <Typography style={{ color: "black" }}>
         에드먼턴 온 교회에 오신 여러분을 환영합니다.<br></br> 교회에 처음 오신
         분들과 개인 사정으로 이주해 오신 분들, 신앙의 회복을 위해 새로이 나아
         오신 분들 모두를 환영합니다.<br></br>새 공동체의 울타리 안에서 새로운
@@ -69,8 +70,8 @@ const NewComers = () => {
         구성원으로 함께 세워져 가길 소원합니다. <br></br>에드먼턴 온 공동체의
         가족이 되심을 기쁨으로 환영합니다. 아래와 같은 과정을 통해 온 공동체의
         가족으로 함께 하게 됩니다.
-      </p>
-      <div style={{ height: "800" }}>
+      </Typography>
+      <div style={{ width: "100%", height: "800px" }}>
         <ReactFlow
           nodes={nodes}
           edges={edges}
