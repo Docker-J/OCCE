f7cecbf7e3c010e0433b1726220c22ee4b2e755b
Junesung <junesoung412@gmail.com>
2023-06-01 15:03:01 -0600

    frontend: online: set frameborder to 0

--- frontend/src/pages/Online/Sermon.js
+++ frontend/src/pages/Online/Sermon.js
@@ -19,6 +19,7 @@ const Sermon = () => {
             height="315"
             src="https://www.youtube.com/embed/videoseries?list=PL-MVshquUXWFkxA1rry9X2d4WAvR-kesH"
             title="YouTube video player"
+            frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
