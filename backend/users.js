67b33e87832fda2dbb912e108dfe0c19d159fc30
Junesung <junesoung412@gmail.com>
2023-06-10 10:57:52 -0600

    backend: user: clean up

--- backend/users.js
+++ backend/users.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
 const {
   CognitoIdentityProviderClient,
   InitiateAuthCommand,
