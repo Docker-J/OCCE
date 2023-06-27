7deb237d448de7b9288add7a52bdf85b3a07cb82
Junesung <junesoung412@gmail.com>
2023-06-27 11:42:28 -0600

    frontend: cleanup

--- frontend/src/components/WeeklyUpdate/BulletinUploadModal.js
+++ frontend/src/components/WeeklyUpdate/BulletinUploadModal.js
@@ -1,31 +1,6 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Box, Button, Modal } from "@mui/material";
 import ButtonDatePicker from "./ButtonDatePicker";
-
-const modalStyle = {
-  overlay: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "rgba(255, 255, 255, 0.45)",
-    zIndex: 10,
-  },
-  content: {
-    justifyContent: "center",
-    background: "#ffffff",
-    overflow: "auto",
-    top: "30vh",
-    left: "35vw",
-    right: "35vw",
-    bottom: "30vh",
-    WebkitOverflowScrolling: "touch",
-    borderRadius: "14px",
-    outline: "none",
-    zIndex: 10,
-  },
-};
 
 const style = {
   position: "absolute",
@@ -42,21 +17,24 @@ const style = {
 };
 
 const BulletinUploadModal = (props) => {
-  function nextSunday() {
-    var today = new Date();
-    today.setDate(today.getDate() + ((-1 - today.getDay() + 7) % 7) + 1);
-    return today;
-  }
+  const [selectedDate, setSelectedDate] = useState();
+  const [fileToUpload, setFileToUpload] = useState(null);
 
-  const [selectedDate, setSelectedDate] = useState(nextSunday());
+  const nextSunday = () => {
+    const today = new Date();
+    today.setDate(today.getDate() + ((-1 - today.getDay() + 7) % 7) + 1);
+    setSelectedDate(today);
+  };
 
   const inputRef = useRef(null);
-
-  const [fileToUpload, setFileToUpload] = useState(null);
 
   const handleChangeFile = ({ target }) => {
     setFileToUpload(target.files[0]);
   };
+
+  useEffect(() => {
+    nextSunday();
+  }, []);
 
   return (
     <Modal open={props.open} onClose={props.onClose}>
