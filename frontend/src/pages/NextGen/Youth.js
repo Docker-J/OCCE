4ed8cf13753ba83110fb357647a72eea291d2a3e
Junesung <junesoung412@gmail.com>
2023-09-10 01:08:39 -0600

    frontend: nextgen: fix wrong asset name

--- frontend/src/pages/NextGen/Youth.js
+++ frontend/src/pages/NextGen/Youth.js
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 const Youth = () => {
   const titleBackground = {
     backgroundImage:
-      'linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)), url("/img/NextGen/3.jpg")',
+      'linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)), url("/img/NextGen/KidsOntheRock.jpg")',
     backgroundPositionY: "52%",
   };
   return (
