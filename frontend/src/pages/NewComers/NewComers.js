7ca4f25f3cc4063ebe5790e3120bd6d271c045b2
Junesung <junesoung412@gmail.com>
2023-11-14 18:17:57 -0700

    frontend: newcomers: hover action

--- frontend/src/pages/NewComers/NewComers.js
+++ frontend/src/pages/NewComers/NewComers.js
@@ -1,118 +1,117 @@
 import { useCallback, useEffect, useState } from "react";
 
-import ReactFlow, { useNodesState, useEdgesState } from "reactflow";
+// import ReactFlow, { useNodesState, useEdgesState } from "reactflow";
 
-import "reactflow/dist/style.css";
+// import "reactflow/dist/style.css";
 import { Typography } from "@mui/material";
-import { useWindowSize } from "@uidotdev/usehooks";
 
 import "./NewComers.css";
 import NewComersTimeline from "./NewComersTimeline";
 
-const initialNodes = [
-  {
-    id: "1",
-    type: "input",
-    position: { x: 0, y: 0 },
-    draggable: false,
-    deletable: false,
-    data: { label: "방문카드 작성" },
-    style: { width: 300 },
-  },
-  {
-    id: "2",
-    position: { x: 0, y: 75 },
-    draggable: false,
-    deletable: false,
-    data: {
-      label: "등록 \n QR코드 온라인 등록 또는 오프라인 등록 양식 작성",
-    },
-    style: { width: 300 },
-  },
-  {
-    id: "3",
-    position: { x: 0, y: 160 },
-    draggable: false,
-    deletable: false,
-    data: {
-      label: "새가족 교육 \n 4주 과정, 주일 오후 4시 15분 새가족실(1층 미팅룸)",
-    },
-    style: { width: 300 },
-  },
-  {
-    id: "4",
-    position: { x: 0, y: 250 },
-    draggable: false,
-    deletable: false,
-    data: { label: "소그룹(정원) 배치" },
-    style: { width: 300 },
-  },
-  {
-    id: "5",
-    type: "output",
-    position: { x: 0, y: 325 },
-    draggable: false,
-    deletable: false,
-    data: { label: "새가족 환영회" },
-    style: { width: 300 },
-  },
-];
+// const initialNodes = [
+//   {
+//     id: "1",
+//     type: "input",
+//     position: { x: 0, y: 0 },
+//     draggable: false,
+//     deletable: false,
+//     data: { label: "방문카드 작성" },
+//     style: { width: 300 },
+//   },
+//   {
+//     id: "2",
+//     position: { x: 0, y: 75 },
+//     draggable: false,
+//     deletable: false,
+//     data: {
+//       label: "등록 \n QR코드 온라인 등록 또는 오프라인 등록 양식 작성",
+//     },
+//     style: { width: 300 },
+//   },
+//   {
+//     id: "3",
+//     position: { x: 0, y: 160 },
+//     draggable: false,
+//     deletable: false,
+//     data: {
+//       label: "새가족 교육 \n 4주 과정, 주일 오후 4시 15분 새가족실(1층 미팅룸)",
+//     },
+//     style: { width: 300 },
+//   },
+//   {
+//     id: "4",
+//     position: { x: 0, y: 250 },
+//     draggable: false,
+//     deletable: false,
+//     data: { label: "소그룹(정원) 배치" },
+//     style: { width: 300 },
+//   },
+//   {
+//     id: "5",
+//     type: "output",
+//     position: { x: 0, y: 325 },
+//     draggable: false,
+//     deletable: false,
+//     data: { label: "새가족 환영회" },
+//     style: { width: 300 },
+//   },
+// ];
 
-const initialEdges = [
-  {
-    id: "e1-2",
-    deletable: false,
-    animated: true,
-    focusable: false,
-    markerEnd: { type: "arrow" },
-    source: "1",
-    target: "2",
-  },
-  {
-    id: "e2-3",
-    deletable: false,
-    animated: true,
-    focusable: false,
-    markerEnd: { type: "arrow" },
-    source: "2",
-    target: "3",
-  },
-  {
-    id: "e3-4",
-    deletable: false,
-    animated: true,
-    focusable: false,
-    markerEnd: { type: "arrow" },
-    source: "3",
-    target: "4",
-  },
-  {
-    id: "e4-5",
-    deletable: false,
-    animated: true,
-    focusable: false,
-    markerEnd: { type: "arrow" },
-    source: "4",
-    target: "5",
-  },
-];
+// const initialEdges = [
+//   {
+//     id: "e1-2",
+//     deletable: false,
+//     animated: true,
+//     focusable: false,
+//     markerEnd: { type: "arrow" },
+//     source: "1",
+//     target: "2",
+//   },
+//   {
+//     id: "e2-3",
+//     deletable: false,
+//     animated: true,
+//     focusable: false,
+//     markerEnd: { type: "arrow" },
+//     source: "2",
+//     target: "3",
+//   },
+//   {
+//     id: "e3-4",
+//     deletable: false,
+//     animated: true,
+//     focusable: false,
+//     markerEnd: { type: "arrow" },
+//     source: "3",
+//     target: "4",
+//   },
+//   {
+//     id: "e4-5",
+//     deletable: false,
+//     animated: true,
+//     focusable: false,
+//     markerEnd: { type: "arrow" },
+//     source: "4",
+//     target: "5",
+//   },
+// ];
 
 const NewComers = () => {
-  const [nodes] = useNodesState(initialNodes);
-  const [edges] = useEdgesState(initialEdges);
+  // const [nodes] = useNodesState(initialNodes);
+  // const [edges] = useEdgesState(initialEdges);
 
-  const size = useWindowSize();
-  const [reactFlowInstance, setReactFlowInstance] = useState(null);
+  // const size = useWindowSize();
+  // const [reactFlowInstance, setReactFlowInstance] = useState(null);
 
-  const onInit = useCallback((reactFlowInstance) => {
-    setReactFlowInstance(reactFlowInstance);
-  }, []);
+  // const onInit = useCallback((reactFlowInstance) => {
+  //   setReactFlowInstance(reactFlowInstance);
+  // }, []);
 
-  useEffect(() => {
-    if (reactFlowInstance) {
-      reactFlowInstance.fitView();
-    }
-  }, [size, reactFlowInstance]);
+  // useEffect(() => {
+  //   if (reactFlowInstance) {
+  //     reactFlowInstance.fitView();
+  //   }
+  // }, [size, reactFlowInstance]);
 
   return (
     <div style={{ margin: "1em" }}>
--- frontend/src/pages/NewComers/NewComersTimeline.js
+++ frontend/src/pages/NewComers/NewComersTimeline.js
@@ -10,116 +10,119 @@ import { useState } from "react";
 import { Popover, Typography } from "@mui/material";
 
 const NewComersTimeline = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorEl, setAnchorEl] = useState({ anchor: null, popoverID: null });
 
-  const handlePopoverOpen = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handlePopoverOpen = (event, popoverID) => {
+    setAnchorEl({ anchor: event.currentTarget, popoverID: popoverID });
   };
 
   const handlePopoverClose = () => {
-    setAnchorEl(null);
+    setAnchorEl({ anchor: null, popoverID: null });
   };
 
-  const openRegister = Boolean(anchorEl);
-  const openDiscipline = Boolean(anchorEl);
+  const openRegister = anchorEl.popoverID === 1 && anchorEl.anchor;
+  const openDiscipline = anchorEl.popoverID === 2 && anchorEl.anchor;
 
   return (
-    <Timeline position="alternate">
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="primary" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        {/* <Tooltip disableFocusListener title="Add"> */}
-        <TimelineContent>방문카드 작성</TimelineContent>
-        {/* </Tooltip> */}
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="primary" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Typography
-            onMouseEnter={handlePopoverOpen}
-            onMouseLeave={handlePopoverClose}
+    <>
+      <Timeline position="alternate">
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineDot variant="outlined" color="primary" />
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent>방문카드 작성</TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineDot variant="outlined" color="primary" />
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent>
+            <Typography
+              onMouseEnter={(e) => handlePopoverOpen(e, 1)}
+              onMouseLeave={handlePopoverClose}
+            >
+              등록 <HelpOutlineIcon fontSize="small" color="primary" />
+            </Typography>
+
+            <Popover
+              id="mouse-over-popover"
+              sx={{
+                pointerEvents: "none",
+              }}
+              open={openRegister}
+              anchorEl={anchorEl.anchor}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              onClose={handlePopoverClose}
+              disableRestoreFocus
+              disableScrollLock
+            >
+              <Typography sx={{ p: 1 }}>
+                QR코드 온라인 등록 또는 오프라인 등록 양식 작성
+              </Typography>
+            </Popover>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineDot variant="outlined" color="primary" />
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent>
+            <Typography
+              onMouseEnter={(e) => handlePopoverOpen(e, 2)}
+              onMouseLeave={handlePopoverClose}
+            >
+              새가족 교육 <HelpOutlineIcon fontSize="small" color="primary" />
+            </Typography>
+          </TimelineContent>
+          <Popover
+            id="mouse-over-popover2"
+            sx={{
+              pointerEvents: "none",
+            }}
+            open={openDiscipline}
+            anchorEl={anchorEl.anchor}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            onClose={handlePopoverClose}
+            disableRestoreFocus
+            disableScrollLock
           >
-            등록 <HelpOutlineIcon fontSize="small" color="primary" />
-          </Typography>
-        </TimelineContent>
-        <Popover
-          id="mouse-over-popover"
-          sx={{
-            pointerEvents: "none",
-          }}
-          open={openRegister}
-          anchorEl={anchorEl}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "center",
-          }}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "left",
-          }}
-          onClose={handlePopoverClose}
-          disableRestoreFocus
-        >
-          <Typography sx={{ p: 1 }}>
-            QR코드 온라인 등록 또는 오프라인 등록 양식 작성
-          </Typography>
-        </Popover>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="primary" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-          <Typography
-            onMouseEnter={handlePopoverOpen}
-            onMouseLeave={handlePopoverClose}
-          >
-            새가족 교육 <HelpOutlineIcon fontSize="small" color="primary" />
-          </Typography>
-        </TimelineContent>
-        <Popover
-          id="mouse-over-popover2"
-          sx={{
-            pointerEvents: "none",
-          }}
-          open={openDiscipline}
-          anchorEl={anchorEl}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "center",
-          }}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "left",
-          }}
-          onClose={handlePopoverClose}
-          disableRestoreFocus
-        >
-          <Typography sx={{ p: 1 }}>
-            4주 과정, 주일 오후 4시 15분 새가족실(1층 미팅룸)
-          </Typography>
-        </Popover>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="primary" />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>소그룹(정원) 배치</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot variant="outlined" color="primary" />
-        </TimelineSeparator>
-        <TimelineContent>새가족 환영회</TimelineContent>
-      </TimelineItem>
-    </Timeline>
+            <Typography sx={{ p: 1 }}>
+              4주 과정, 주일 오후 4시 15분 새가족실(1층 미팅룸)
+            </Typography>
+          </Popover>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineDot variant="outlined" color="primary" />
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent>소그룹(정원) 배치</TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineDot variant="outlined" color="primary" />
+          </TimelineSeparator>
+          <TimelineContent>새가족 환영회</TimelineContent>
+        </TimelineItem>
+      </Timeline>
+    </>
   );
 };
 
