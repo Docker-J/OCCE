729068a50c95169bf7ac31037d0bdab78848670f
Junesung <junesoung412@gmail.com>
2023-11-16 15:07:21 -0700

    frontend: newcomers: cleanup

--- frontend/src/pages/NewComers/NewComersTimeline.js
+++ frontend/src/pages/NewComers/NewComersTimeline.js
@@ -24,105 +24,103 @@ const NewComersTimeline = () => {
   const openDiscipline = anchorEl.popoverID === 2 && anchorEl.anchor;
 
   return (
-    <>
-      <Timeline position="alternate">
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot variant="outlined" color="primary" />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>방문카드 작성</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot variant="outlined" color="primary" />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography
-              onMouseEnter={(e) => handlePopoverOpen(e, 1)}
-              onMouseLeave={handlePopoverClose}
-            >
-              등록 <HelpOutlineIcon fontSize="small" color="primary" />
-            </Typography>
+    <Timeline position="alternate">
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot variant="outlined" color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>방문카드 작성</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot variant="outlined" color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Typography
+            onMouseEnter={(e) => handlePopoverOpen(e, 1)}
+            onMouseLeave={handlePopoverClose}
+          >
+            등록 <HelpOutlineIcon fontSize="small" color="primary" />
+          </Typography>
 
-            <Popover
-              id="mouse-over-popover"
-              sx={{
-                pointerEvents: "none",
-              }}
-              open={openRegister}
-              anchorEl={anchorEl.anchor}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "right",
-              }}
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              onClose={handlePopoverClose}
-              disableRestoreFocus
-              disableScrollLock
-            >
-              <Typography sx={{ p: 1 }}>
-                QR코드 온라인 등록 또는 오프라인 등록 양식 작성
-              </Typography>
-            </Popover>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot variant="outlined" color="primary" />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography
-              onMouseEnter={(e) => handlePopoverOpen(e, 2)}
-              onMouseLeave={handlePopoverClose}
-            >
-              새가족 교육 <HelpOutlineIcon fontSize="small" color="primary" />
-            </Typography>
-          </TimelineContent>
           <Popover
-            id="mouse-over-popover2"
+            id="mouse-over-popover"
             sx={{
               pointerEvents: "none",
             }}
-            open={openDiscipline}
+            open={openRegister}
             anchorEl={anchorEl.anchor}
             anchorOrigin={{
               vertical: "bottom",
-              horizontal: "left",
+              horizontal: "right",
             }}
             transformOrigin={{
               vertical: "top",
-              horizontal: "left",
+              horizontal: "right",
             }}
             onClose={handlePopoverClose}
             disableRestoreFocus
             disableScrollLock
           >
             <Typography sx={{ p: 1 }}>
-              4주 과정, 주일 오후 4시 15분 새가족실(1층 미팅룸)
+              QR코드 온라인 등록 또는 오프라인 등록 양식 작성
             </Typography>
           </Popover>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot variant="outlined" color="primary" />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>소그룹(정원) 배치</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot variant="outlined" color="primary" />
-          </TimelineSeparator>
-          <TimelineContent>새가족 환영회</TimelineContent>
-        </TimelineItem>
-      </Timeline>
-    </>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot variant="outlined" color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Typography
+            onMouseEnter={(e) => handlePopoverOpen(e, 2)}
+            onMouseLeave={handlePopoverClose}
+          >
+            새가족 교육 <HelpOutlineIcon fontSize="small" color="primary" />
+          </Typography>
+        </TimelineContent>
+        <Popover
+          id="mouse-over-popover2"
+          sx={{
+            pointerEvents: "none",
+          }}
+          open={openDiscipline}
+          anchorEl={anchorEl.anchor}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          onClose={handlePopoverClose}
+          disableRestoreFocus
+          disableScrollLock
+        >
+          <Typography sx={{ p: 1 }}>
+            4주 과정, 주일 오후 4시 15분 새가족실(1층 미팅룸)
+          </Typography>
+        </Popover>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot variant="outlined" color="primary" />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>소그룹(정원) 배치</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot variant="outlined" color="primary" />
+        </TimelineSeparator>
+        <TimelineContent>새가족 환영회</TimelineContent>
+      </TimelineItem>
+    </Timeline>
   );
 };
 
