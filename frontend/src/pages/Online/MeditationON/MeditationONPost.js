9c64a6c7fea02834c89806da2f04d251b9aa2eb5
Docker-J <junesoung412@gmail.com>
2024-01-02 21:08:53 -0700

    frontend: online: meditationON: fix post layout

--- frontend/src/pages/Online/MeditationON/MeditationONPost.js
+++ frontend/src/pages/Online/MeditationON/MeditationONPost.js
@@ -2,8 +2,8 @@ import { useLoaderData } from "react-router-dom";
 
 import { Typography } from "@mui/material";
 
-import "../../NextGen/NextGen.css";
 import CustomCarousel from "../../../common/CustomCarousel";
+import "../../NextGen/NextGen.css";
 
 const titleBackground = {
   backgroundImage:
@@ -15,8 +15,8 @@ const MeditationONPost = () => {
 
   return (
     <>
-      <div className="title" style={titleBackground}>
-        <div className="titleContent">
+      <div className="title-wrapper" style={titleBackground}>
+        <div className="title">
           <Typography
             variant="h4"
             fontWeight={830}
@@ -26,40 +26,45 @@ const MeditationONPost = () => {
           </Typography>
         </div>
       </div>
-      <div
-        className="carousel-container"
-        style={{
-          // display: "flex",
-          // flexDirection: "row",
-          // alignItems: "center",
-          position: "relative",
-          width: "95%",
-          maxWidth: "800px",
-          left: "50%",
-          transform: "translateX(-50%)",
-          marginTop: "30px",
-          marginBottom: "30px",
-        }}
-      >
-        <CustomCarousel>
-          {Object.values(images).map((image, index) => {
-            // if (
-            //   index === selectedItem ||
-            //   index === selectedItem + 1 ||
-            //   index === selectedItem - 1
-            // ) {
-            return (
-              <img
-                style={{ objectFit: "contain" }}
-                key={index}
-                src={`https://imagedelivery.net/ICo2WI8PXO_BVRlWfwzOww/${image}/MeditationON`}
-                alt="test"
-                loading="lazy"
-              />
-            );
-            // } else return <></>;
-          })}
-        </CustomCarousel>
+
+      <div className="container-wrapper">
+        <div className="container">
+          <div
+            className="carousel-container"
+            style={{
+              // display: "flex",
+              // flexDirection: "row",
+              // alignItems: "center",
+              position: "relative",
+              width: "95%",
+              maxWidth: "800px",
+              left: "50%",
+              transform: "translateX(-50%)",
+              marginTop: "30px",
+              marginBottom: "30px",
+            }}
+          >
+            <CustomCarousel>
+              {Object.values(images).map((image, index) => {
+                // if (
+                //   index === selectedItem ||
+                //   index === selectedItem + 1 ||
+                //   index === selectedItem - 1
+                // ) {
+                return (
+                  <img
+                    style={{ objectFit: "contain" }}
+                    key={index}
+                    src={`https://imagedelivery.net/ICo2WI8PXO_BVRlWfwzOww/${image}/MeditationON`}
+                    alt="test"
+                    loading="lazy"
+                  />
+                );
+                // } else return <></>;
+              })}
+            </CustomCarousel>
+          </div>
+        </div>
       </div>
     </>
   );
