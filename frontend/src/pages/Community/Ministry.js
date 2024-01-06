4d52d042894fbe9f8359d40caa6d780f0951cdab
Docker-J <junesoung412@gmail.com>
2024-01-06 11:47:44 -0700

    frontend: community: ministry: cleaner code

--- frontend/src/pages/Community/Ministry.js
+++ frontend/src/pages/Community/Ministry.js
@@ -1,6 +1,7 @@
 import { List, ListItem, Typography } from "@mui/material";
 import "../NextGen/NextGen.css";
 import { Link } from "react-router-dom";
+import { MinistryList } from "./MinistryList";
 
 const titleBackground = {
   backgroundImage:
@@ -34,7 +35,9 @@ const Ministry = () => {
             소그룹 <Link to="/community/smallgroup">'정원' </Link>이 순환하여
             섬깁니다.
           </Typography>
+
           <br />
+
           <Typography variant="h5" fontWeight={800}>
             온 교회 사역 지원
           </Typography>
@@ -42,12 +45,7 @@ const Ministry = () => {
             교회에 필요한 사역 지원을 수시로 받고 있습니다. 허락하신 달란트대로
             주의 몸 된 교회를 함께 세우고, 온 맘과 온 힘을 다해 하나님과 이웃을
             사랑하는 공동체가 되길 소망합니다. 아래의 사역 분야를 참고해 주시고,
-            온라인 링크를 통하여 지원해 주시면 감사하겠습니다. <br />
-            {/* 교육부서 Staff -유아유치부 -유초등부 -중고등부 중보기도팀 찬양팀
-          -찬양인도 -보컬 -피아노 -키보드 -통기타 -일렉기타 -베이스기타 -드럼
-          -바이올린 -플룻 -others 미디어팀 -사운드믹서 운용 -PPT/조명/비디오믹서
-          운용 -PPT 제작 -사진/비디오 촬영 -동영상 제작 -포스터 제작 및 각종
-          디자인 */}
+            온라인 링크를 통하여 지원해 주시면 감사하겠습니다.
             <div
               style={{
                 display: "flex",
@@ -56,43 +54,20 @@ const Ministry = () => {
                 flex: 1,
               }}
             >
-              <List>
-                <ListItem>교육부서</ListItem>
+              {MinistryList.map((ministry) => (
                 <List>
-                  <ListItem>유아유치부</ListItem>
-                  <ListItem>유초등부</ListItem>
-                  <ListItem>중고등부</ListItem>
+                  <ListItem>{ministry.title}</ListItem>
+                  <List>
+                    {ministry.types.map((type) => (
+                      <ListItem>{type}</ListItem>
+                    ))}
+                  </List>
                 </List>
-              </List>
-              <List>
-                <ListItem>찬양팀</ListItem>
-                <List>
-                  <ListItem>찬양인도 보컬</ListItem>
-                  <ListItem>피아노 키보드</ListItem>
-                  <ListItem>통기타 일렉기타</ListItem>
-                  <ListItem>베이스기타 드럼</ListItem>
-                  <ListItem>바이올린 플룻</ListItem>
-                  <ListItem>Others</ListItem>
-                </List>
-              </List>
-              <List>
-                <ListItem>미디어팀</ListItem>
-                <List>
-                  <ListItem>사운드믹서 운용</ListItem>
-                  <ListItem>PPT/조명/비디오믹서 운용</ListItem>
-                  <ListItem>PPT 제작</ListItem>
-                  <ListItem>사진/비디오 촬영</ListItem>
-                  <ListItem>동영상 제작</ListItem>
-                  <ListItem>포스터 제작 및 각종 디자인</ListItem>
-                </List>
-              </List>
-              <List>
-                <ListItem>중보기도팀</ListItem>
-              </List>
+              ))}
             </div>
             <br />
             <a target="__blank" href="https://forms.gle/5kGFLfA5fhfotVTW6">
-              온라인 사역 지원 링크 바로가기
+              사역 지원하기
             </a>
           </Typography>
         </div>
