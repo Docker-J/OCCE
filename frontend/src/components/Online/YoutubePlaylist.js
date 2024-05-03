cd4bd6a14fbbefe87be9a5b9e89b18d72992329a
Docker-J <junesoung412@gmail.com>
2024-05-03 15:23:33 -0600

    frontend: online: use css border instead of frameborder

--- frontend/src/components/Online/YoutubePlaylist.js
+++ frontend/src/components/Online/YoutubePlaylist.js
@@ -7,7 +7,6 @@ const YoutubePlaylist = ({ src }) => {
         <iframe
           src={src}
           title="YouTube video player"
-          frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
