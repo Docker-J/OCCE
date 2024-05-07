2ad0f71119de70e3048292bd745ad0caffe9eaa5
Docker-J <junesoung412@gmail.com>
2024-05-06 20:44:42 -0600

    backend: cleanup

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,15 +1,13 @@
 import { CognitoJwtVerifier } from "aws-jwt-verify";
 
-const AWS_COGNITO_CLIENT_ID = process.env.AWS_COGNITO_CLIENT_ID;
-
 const verifier = CognitoJwtVerifier.create({
   userPoolId: process.env.AWS_COGNITO_USER_POOL_ID,
   tokenUse: "access",
-  clientId: AWS_COGNITO_CLIENT_ID,
+  clientId: process.env.AWS_COGNITO_CLIENT_ID,
   groups: "Staff",
 });
 
-const authStaff = async (req, res, next) => {
+const authStaff = async (req, _, next) => {
   // 바로
   try {
     const token = req.header("Authorization").split(" ")[1];
