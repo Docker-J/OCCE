f07db3b5232d5d2a98dc49e3e8af82a763eb7cd6
Docker-J <junesoung412@gmail.com>
2024-05-10 17:44:21 -0600

    backend: auth: add user verifier

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,17 +1,38 @@
 import { CognitoJwtVerifier } from "aws-jwt-verify";
 
-const verifier = CognitoJwtVerifier.create({
+const staffVerifier = CognitoJwtVerifier.create({
   userPoolId: process.env.AWS_COGNITO_USER_POOL_ID,
   tokenUse: "access",
   clientId: process.env.AWS_COGNITO_CLIENT_ID,
   groups: "Staff",
 });
 
+const userVerfier = CognitoJwtVerifier.create({
+  userPoolId: process.env.AWS_COGNITO_USER_POOL_ID,
+  tokenUse: "access",
+  clientId: process.env.AWS_COGNITO_CLIENT_ID,
+});
+
 const authStaff = async (req, _, next) => {
   // 바로
   try {
     const token = req.header("Authorization").split(" ")[1];
-    const payload = await verifier.verify(token);
+    const payload = await staffVerifier.verify(token);
+    console.log("Token is valid. Payload:", payload);
+
+    next();
+  } catch (err) {
+    console.log("Token not valid!");
+
+    next(err);
+  }
+};
+
+const authUser = async (req, _, next) => {
+  // 바로
+  try {
+    const token = req.header("Authorization").split(" ")[1];
+    const payload = await userVerfier.verify(token);
     console.log("Token is valid. Payload:", payload);
 
     next();
