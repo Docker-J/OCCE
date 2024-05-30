431ef6dc37f484717281a8e0366606b03e7ae6f6
Docker-J <junesoung412@gmail.com>
2024-05-29 19:54:11 -0600

    frontend: common: CustomModal component

---
+++ frontend/src/common/CustomModal.js
@@ -0,0 +1,39 @@
+import { Box, IconButton, Modal } from "@mui/material";
+
+import CloseIcon from "@mui/icons-material/Close";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80vw",
+  bgcolor: "#ffffff",
+  boxShadow: 24,
+  borderRadius: "0.5em",
+  p: 1,
+  py: 5,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const CustomModal = ({ isOpen, onClose, maxWidth, children }) => {
+  return (
+    <Modal open={isOpen} onClose={onClose}>
+      <Box sx={style} maxWidth={maxWidth}>
+        <IconButton
+          onClick={onClose}
+          sx={{ position: "absolute", top: 12, right: 12 }}
+        >
+          <CloseIcon />
+        </IconButton>
+
+        {children}
+      </Box>
+    </Modal>
+  );
+};
+
+export default CustomModal;
