28ddb2bb8835221421ab716b879bc1555619fc83
Docker-J <junesoung412@gmail.com>
2024-11-21 13:08:19 -0700

    frontend: about: update service and gathering info

--- frontend/src/components/AboutUs/ServiceAndGatheringInfo.js
+++ frontend/src/components/AboutUs/ServiceAndGatheringInfo.js
@@ -6,7 +6,7 @@ export const ServiceAndGatheringInfo = [
   },
   {
     title: "새벽 QT",
-    time: "월 - 토요일 오전 6시",
+    time: "월-토요일 오전 6시",
     place: "교회 Youtube 채널",
   },
   {
@@ -16,7 +16,7 @@ export const ServiceAndGatheringInfo = [
   },
   {
     title: "하반기 특별 새벽기도회",
-    time: "8월 마지막주 월-토요일",
+    time: "8월 마지막 주 월-토요일",
     place: "본당",
   },
   {
@@ -35,13 +35,23 @@ export const ServiceAndGatheringInfo = [
     place: "Fireside Room",
   },
   {
-    title: "청년부 주일 모임",
-    time: "오후 4시",
-    place: "Youth Room/Small Group Rooms",
+    title: "청년부 예배",
+    time: "첫째 토요일 오후 5시",
+    place: "본당",
+  },
+  {
+    title: "청년부 정원 모임",
+    time: "둘째, 넷째 주일 오후 4시",
+    place: "Youth Room, 본당",
+  },
+  {
+    title: "청년부 연합 정원 모임",
+    time: "셋째 금요일 오후 6시",
+    place: "문의: 부장 집사",
   },
   {
     title: "중보기도 모임",
-    time: " 주일 오후 1시 50분, 월 오전 10시",
-    place: "문의 : 주미옥 장로",
+    time: "주일 오후 1시 50분, 월 오전 10시",
+    place: "문의: 주미옥 장로",
   },
 ];
