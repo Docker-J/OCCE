d7d5abfee329018ccf8ef0f494e9ae89241edad2
Docker-J <junesoung412@gmail.com>
2024-12-04 17:48:52 -0700

    backend: notification: use epoch to use DynamoDB TTL

--- backend/controller/notification.controller.js
+++ backend/controller/notification.controller.js
@@ -3,12 +3,18 @@ import { PutCommand } from "@aws-sdk/lib-dynamodb";
 
 const TABLENAME = "FCMToken";
 
+function getExpirationEpoch() {
+  const now = new Date();
+  const nextMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+  return Math.floor(nextMonth.getTime() / 1000);
+}
+
 export const registerController = async (req, res) => {
   const command = new PutCommand({
     TableName: TABLENAME,
     Item: {
       token: req.body.token,
-      timestamp: new Date().toISOString(),
+      expiresAt: getExpirationEpoch(),
     },
   });
 
