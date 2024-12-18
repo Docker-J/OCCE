a34520df184b35d649db0e3ebd726a6787e28316
Docker-J <junesoung412@gmail.com>
2024-12-17 22:53:29 -0700

    frontend: footer: clean up

--- frontend/src/header/GoogleMap.js
+++ frontend/src/header/GoogleMap.js
@@ -12,7 +12,7 @@ const containerStyle = {
   height: "200px",
 };
 
-const center = {
+const defaultCenter = {
   lat: 53.53120758825804,
   lng: -113.47254286783863,
 };
@@ -25,21 +25,22 @@ const GoogleMap = memo(() => {
   const toggleInfoWindow = () =>
     setInfowindowShown((previousState) => !previousState);
 
-  const closeInfoWindow = () => setInfowindowShown(false);
+  const closeInfoWindow = () => {
+    setInfowindowShown(false);
+  };
 
   return (
     <div style={containerStyle}>
       <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
         <Map
-          // styles={mapStyle}
-          defaultCenter={center}
+          defaultCenter={defaultCenter}
           defaultZoom={14}
           disableDefaultUI={true}
           mapId="96730a2a3f5e9934"
         >
           <AdvancedMarker
             ref={markerRef}
-            position={center}
+            position={defaultCenter}
             onClick={toggleInfoWindow}
           >
             <img
