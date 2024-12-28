1d8d2b9ccfa452502f78cde173580d29c63c4c97
Docker-J <junesoung412@gmail.com>
2024-12-28 02:38:00 -0700

    frontend: update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,12 +11,7 @@ export default defineConfig(() => {
     },
 
     plugins: [
-      react({
-        jsxImportSource: "@emotion/react",
-        babel: {
-          plugins: ["@emotion/babel-plugin"],
-        },
-      }),
+      react(),
       basicSsl(),
       jsconfigPaths(),
       svgLoader({
