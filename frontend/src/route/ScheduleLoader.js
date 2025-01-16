e4ae7b0b9cb195363a8912f564a35ab45a34338f
Docker-J <junesoung412@gmail.com>
2025-01-15 18:20:13 -0700

    frontend: schedule: proper environment variable name

--- frontend/src/route/ScheduleLoader.js
+++ frontend/src/route/ScheduleLoader.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function loader() {
   const getSchedules = axios.get(
     `https://www.googleapis.com/calendar/v3/calendars/${
-      import.meta.env.VITE_APP_GOOGLE_CLANEDAR_ID
+      import.meta.env.VITE_APP_GOOGLE_CALENDAR_ID
     }/events?key=${import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY}`
   );
 
