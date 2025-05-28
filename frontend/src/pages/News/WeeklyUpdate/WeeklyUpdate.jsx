4f3c9da05fbcabb1b4a979881e98792772bc8f32
Docker-J <junesoung412@gmail.com>
2025-05-27 21:18:24 -0600

    frontend: weeklyupdate: clean up

--- frontend/src/pages/News/WeeklyUpdate/WeeklyUpdate.jsx
+++ frontend/src/pages/News/WeeklyUpdate/WeeklyUpdate.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 import { CircularProgress, Fab, IconButton, Typography } from "@mui/material";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
