c57b9c91ba10e35dd78fcf260e138ac730183c56
Junesung Lee <junesoung412@gmail.com>
2026-04-02 23:11:14 -0600

    frontend: no more custom build of ckeditor

--- frontend/src/components/News/Announcement/TextEditor.jsx
+++ frontend/src/components/News/Announcement/TextEditor.jsx
@@ -1,18 +1,121 @@
-import Editor from "ckeditor5-custom-build/build/ckeditor";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
+import {
+  ClassicEditor,
+  Autoformat,
+  Alignment,
+  Base64UploadAdapter,
+  BlockQuote,
+  Bold,
+  Essentials,
+  Font,
+  Heading,
+  Image,
+  ImageCaption,
+  ImageResize,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+  Indent,
+  Italic,
+  Link,
+  List,
+  MediaEmbed,
+  Paragraph,
+  PasteFromOffice,
+  Table,
+  TableToolbar,
+  TextTransformation,
+  Underline,
+} from "ckeditor5";
 
+// Import the core styles directly in your component
+import "ckeditor5/ckeditor5.css";
 import "./TextEditor.css";
 
 const TextEditor = ({ body, getBody }) => {
   return (
-    <CKEditor
-      editor={Editor}
-      data={body}
-      onChange={(_, editor) => {
-        const data = editor.getData();
-        getBody(data);
-      }}
-    />
+      <CKEditor
+        editor={ClassicEditor}
+        data={body}
+        config={{
+          // REQUIRED for v44+ (2025/2026)
+          licenseKey: "GPL",
+          plugins: [
+            Autoformat,
+            Alignment,
+            Base64UploadAdapter,
+            BlockQuote,
+            Bold,
+            Essentials,
+            Font,
+            Heading,
+            Image,
+            ImageCaption,
+            ImageResize,
+            ImageStyle,
+            ImageToolbar,
+            ImageUpload,
+            Indent,
+            Italic,
+            Link,
+            List,
+            MediaEmbed,
+            Paragraph,
+            PasteFromOffice,
+            Table,
+            TableToolbar,
+            TextTransformation,
+            Underline,
+          ],
+          toolbar: {
+            items: [
+              "heading",
+              "|",
+              "bold",
+              "italic",
+              "underline",
+              "fontSize",
+              "fontColor",
+              "fontBackgroundColor",
+              "|",
+              "alignment",
+              "|",
+              "link",
+              "bulletedList",
+              "numberedList",
+              "|",
+              "outdent",
+              "indent",
+              "|",
+              "imageUpload",
+              "blockQuote",
+              "insertTable",
+              "mediaEmbed",
+              "undo",
+              "redo",
+            ],
+          },
+          image: {
+            toolbar: [
+              "toggleImageCaption",
+              "|",
+              "imageStyle:inline",
+              "imageStyle:block",
+              "imageStyle:side",
+            ],
+          },
+          table: {
+            contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+          },
+          mediaEmbed: {
+            previewsInData: true,
+          },
+        }}
+        onChange={(_, editor) => {
+          const data = editor.getData();
+          getBody(data);
+        }}
+      />
   );
 };
 
