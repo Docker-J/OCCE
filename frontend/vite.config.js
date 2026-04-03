f66d6e54e4be53e3440d9afe83657d9304a1d81d
Junesung Lee <junesoung412@gmail.com>
2026-04-02 23:29:21 -0600

    frontend: vite: bump

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 import { analyzer } from "vite-bundle-analyzer";
 
@@ -15,7 +14,6 @@ export default defineConfig(() => {
       react(),
       analyzer(),
       basicSsl(),
-      tsconfigPaths(),
       svgr({
         svgrOptions: {
           exportType: "default",
