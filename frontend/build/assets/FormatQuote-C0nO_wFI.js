85b4528ff513b1ec1433a65f131d11fd158d9b73
Junesung Lee <junesoung412@gmail.com>
2026-07-28 18:26:07 -0600

    frontend: deps: remove redux

---
+++ frontend/build/assets/FormatQuote-C0nO_wFI.js
@@ -0,0 +1,1 @@
+import{t as e}from"./reactVendor-DRVin0oV.js";import{n as t}from"./Paper-CFIV0xyD.js";var n=t((0,e().jsx)(`path`,{d:`M6 17h3l2-4V7H5v6h3zm8 0h3l2-4V7h-6v6h3z`}),`FormatQuote`);export{n as t};
