a51906dee825b2fe68b337378fc2d9db37be3bfb
Junesung Lee <junesoung412@gmail.com>
2026-08-05 21:50:02 -0600

    Fix: Remove cache entirely from bible291 API to ensure it updates exactly at midnight

--- backend/routes/bible291.routes.js
+++ backend/routes/bible291.routes.js
@@ -1,9 +1,9 @@
-import { cache } from "hono/cache";
+
 import { Hono } from "hono";
 import { getTodayBible291Controller } from "../controller/bible291.controller.js";
 
 const router = new Hono();
 
-router.get("/today", cache({ cacheName: "occe-api", cacheControl: "public, s-maxage=3600, max-age=0" }), getTodayBible291Controller);
+router.get("/today", getTodayBible291Controller);
 
 export default router;
